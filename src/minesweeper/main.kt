d699775d9576355a40fc517eb177acaba8544248
Izmaylov Alexandr <zinoviy23@gmail.com>
2019-02-10 00:20:40 +0300

    My generator

--- src/minesweeper/main.kt
+++ src/minesweeper/main.kt
@@ -1,5 +1,22 @@
 package minesweeper
 
+import java.util.*
+import kotlin.random.Random
+
 fun main(args: Array<String>) {
-    print("Hello world!")
+    val scanner = Scanner(System.`in`)
+    print("How many mines do you want on the field? ")
+    val mineCount = scanner.nextInt()
+
+    var needMine = mineCount
+    for (i in 0..9) {
+        for (j in 0..9) {
+            if (needMine > 0 && (Random.nextInt(100) <= mineCount || needMine == 100 - i * 10 - j)) {
+                needMine--
+                print("X")
+            } else
+                print(".")
+        }
+        println()
+    }
 }
