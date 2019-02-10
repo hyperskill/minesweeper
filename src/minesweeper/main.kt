9dcf4f9df29202bc7c5c20bc9b77a2c2aa6ccaed
Izmaylov Alexandr <zinoviy23@gmail.com>
2019-02-10 21:33:54 +0300

    final game

--- src/minesweeper/main.kt
+++ src/minesweeper/main.kt
@@ -3,56 +3,91 @@ package minesweeper
 import java.util.*
 import kotlin.random.Random
 
-fun generateMines(field: Array<CharArray>, mineCount: Int) {
-    var needMine = mineCount
-    for (i in 0..9) {
-        for (j in 0..9) {
-            if (needMine > 0 && (Random.nextInt(100) <= mineCount || needMine == 100 - i * 10 - j)) {
+class FieldInfo(val field: Array<CharArray>,
+                val size: Pair<Int, Int>,
+                val mineCount: Int,
+                val marks: MutableSet<Pair<Int, Int>>,
+                val opened: MutableSet<Pair<Int, Int>>)
+
+fun generateMines(fieldInfo: FieldInfo, generationPoint: Pair<Int, Int>) {
+    var needMine = fieldInfo.mineCount
+    for (i in 0 until fieldInfo.size.first) {
+        for (j in 0 until fieldInfo.size.second) {
+            if (i == generationPoint.first && j == generationPoint.second) {
+                if (needMine < fieldInfo.size.first * fieldInfo.size.second - i * fieldInfo.size.second - j)
+                    continue
+                else {
+                    val firstNotMine = findFirstNoMine(fieldInfo)
+                    fieldInfo.field[firstNotMine.first][firstNotMine.second] = 'X'
+                    needMine--
+                    continue
+                }
+            }
+
+            if (needMine > 0
+                    && (Random.nextInt(fieldInfo.size.first * fieldInfo.size.second - 1) <= fieldInfo.mineCount
+                    || needMine == fieldInfo.size.first * fieldInfo.size.second - i * fieldInfo.size.second - j))
+            {
                 needMine--
-                field[i][j] = 'X'
+                fieldInfo.field[i][j] = 'X'
             } else
-                field[i][j] = '.'
+                fieldInfo.field[i][j] = '.'
         }
     }
 }
 
-fun countMines(field: Array<CharArray>) {
-    for (i in 0..9)
-        for (j in 0..9)
-            if (field[i][j] != 'X') {
-                val mineCount = countMinesAroundCell(field, i, j)
+fun findFirstNoMine(fieldInfo: FieldInfo): Pair<Int, Int> {
+    for (i in 0 until fieldInfo.size.first)
+        for (j in 0 until fieldInfo.size.second)
+            if (fieldInfo.field[i][j] != 'X')
+                return Pair(i, j)
+
+    return Pair(-1, -1)
+}
+
+fun countMines(fieldInfo: FieldInfo) {
+    for (i in 0 until fieldInfo.size.first)
+        for (j in 0 until fieldInfo.size.second)
+            if (fieldInfo.field[i][j] != 'X') {
+                val mineCount = countMinesAroundCell(fieldInfo, i, j)
                 if (mineCount != 0)
-                    field[i][j] = '0' + mineCount
+                    fieldInfo.field[i][j] = '0' + mineCount
+                else
+                    fieldInfo.field[i][j] = '\\'
             }
 }
 
-fun countMinesAroundCell(field: Array<CharArray>, i: Int, j: Int): Int {
+fun countMinesAroundCell(fieldInfo: FieldInfo, i: Int, j: Int): Int {
     val delta = intArrayOf(-1, 0, 1)
 
     var cnt = 0
     for (dx in delta)
         for (dy in delta)
-            if (!(dx == 0 && dy == 0) && i + dx >= 0 && dx + i < 10 && j + dy >= 0 && j + dy < 10 &&
-                    field[dx + i][dy + j] == 'X')
+            if (!(dx == 0 && dy == 0)
+                    && i + dx in 0 until fieldInfo.size.first
+                    && j + dy in 0 until fieldInfo.size.second
+                    && fieldInfo.field[dx + i][dy + j] == 'X')
                 cnt++
 
     return cnt
 }
 
-fun printField(field: Array<CharArray>, marks: List<Pair<Int, Int>>) {
+fun printField(fieldInfo: FieldInfo, show: Boolean) {
 
-    val fieldToPrint = field.map { it.copyOf() }.toTypedArray()
+    val fieldToPrint = fieldInfo.field.map { it.copyOf() }.toTypedArray()
 
-    for ((i, j) in marks) {
-        fieldToPrint[i][j] = '*'
+    if (!show) {
+        for (i in 0 until fieldInfo.size.first)
+            for (j in 0 until fieldInfo.size.second)
+                if (Pair(i, j) !in fieldInfo.opened)
+                    fieldToPrint[i][j] = '.'
+
+        for ((i, j) in fieldInfo.marks) {
+            fieldToPrint[i][j] = '*'
+        }
     }
 
-    for (i in 0..9)
-        for (j in 0..9)
-            if (fieldToPrint[i][j] == 'X')
-                fieldToPrint[i][j] = '.'
-
-    println(" |${('a'..('a' + 9)).joinToString("")}|")
+    println(" |${('a'..('a' + fieldInfo.size.second - 1)).joinToString("")}|")
     println("-|----------|")
     var ind = 0
     println(fieldToPrint.joinToString("\n") {
@@ -61,39 +96,113 @@ fun printField(field: Array<CharArray>, marks: List<Pair<Int, Int>>) {
     println("-|----------|")
 }
 
-fun checkGame(field: Array<CharArray>, marks: List<Pair<Int, Int>>, mineCount: Int): Boolean {
-    if (marks.size != mineCount)
+fun openCell(fieldInfo: FieldInfo, point: Pair<Int, Int>): Boolean {
+    if (point in fieldInfo.opened)
         return false
 
-    for ((i, j) in marks) {
-        if (field[i][j] != 'X')
+    if (fieldInfo.field[point.first][point.second] == 'X')
+        return true
+
+    if (point in fieldInfo.marks)
+        fieldInfo.marks -= point
+
+    fieldInfo.opened += point
+    if (fieldInfo.field[point.first][point.second] == '\\')
+        openNeighbours(fieldInfo, point)
+
+    return false
+}
+
+fun openNeighbours(fieldInfo: FieldInfo, point: Pair<Int, Int>) {
+    val delta = intArrayOf(-1, 0, 1)
+
+    for (dx in delta)
+        for (dy in delta) {
+            if (point.first + dx !in 0 until fieldInfo.size.first
+                    || point.second + dy !in 0 until fieldInfo.size.second)
+                continue
+
+            if (Pair(point.first + dx, point.second + dy) !in fieldInfo.opened)
+                openCell(fieldInfo, Pair(point.first + dx, point.second + dy))
+        }
+}
+
+fun checkGame(fieldInfo: FieldInfo): Boolean {
+    if (fieldInfo.opened.size == fieldInfo.size.first * fieldInfo.size.second - fieldInfo.mineCount)
+        return true
+
+    if (fieldInfo.marks.size != fieldInfo.mineCount)
+        return false
+
+    for ((i, j) in fieldInfo.marks) {
+        if (fieldInfo.field[i][j] != 'X')
             return false
     }
 
     return true
 }
 
-fun main(args: Array<String>) {
+fun mark(fieldInfo: FieldInfo, point: Pair<Int, Int>) {
+    if (point !in fieldInfo.opened)
+        fieldInfo.marks += point
+}
+
+fun main() {
     val scanner = Scanner(System.`in`)
-    print("How many mines do you want on the field? ")
-    val mineCount = scanner.nextInt()
+    var mineCount: Int
 
-    val field = Array(10) { CharArray(10) }
-    generateMines(field, mineCount)
-    countMines(field)
+    do {
+        print("How many mines do you want on the field? ")
+        mineCount = scanner.nextInt()
+    } while (mineCount !in 1..99)
 
-    printField(field, listOf())
+    val fieldInfo = FieldInfo(
+            Array(10) { CharArray(10) },
+            Pair(10, 10),
+            mineCount,
+            mutableSetOf(),
+            mutableSetOf()
+    )
 
-    val marks = mutableListOf<Pair<Int, Int>>()
-    while (!checkGame(field, marks, mineCount)) {
-        print("Set/delete mines marks (x and y coordinates): ")
+    printField(fieldInfo, false)
+    var generationPoint: Pair<Int, Int>
+    do {
+        print("Choose first cell to free: ")
         val x = scanner.next().first()
         val y = scanner.next().first()
+        generationPoint = Pair(y - 'a', x - 'a')
+    } while (generationPoint.first !in 0 until fieldInfo.size.first
+            || generationPoint.second !in 0 until fieldInfo.size.second)
 
-        marks += Pair(y - 'a', x - 'a')
-        printField(field, marks)
+    generateMines(fieldInfo, generationPoint)
+    countMines(fieldInfo)
+    openCell(fieldInfo, generationPoint)
+    printField(fieldInfo, false)
+
+    var isFail = false
+    while (!checkGame(fieldInfo) && !isFail) {
+        print("Set/unset mines marks or claim a cell as free: ")
+        val x = scanner.next().first()
+        val y = scanner.next().first()
+        val cmd = scanner.next()
+
+        val point = Pair(y - 'a', x - 'a')
+
+        if (point.first in 0 until fieldInfo.size.first && point.second in 0 until fieldInfo.size.second) {
+            isFail = when (cmd) {
+                "free" -> openCell(fieldInfo, point)
+                "mark" -> {
+                    mark(fieldInfo, point)
+                    false
+                }
+                else -> false
+            }
+        }
+        printField(fieldInfo, isFail)
     }
 
-    println("Congratulations! You founded all mines!")
-//    println(field.joinToString("\n") { it.joinToString("") })
+    if (!isFail)
+        println("Congratulations! You founded all mines!")
+    else
+        println("You stepped on a mine and failed!")
 }
