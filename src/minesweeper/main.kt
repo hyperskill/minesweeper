95121c4c8960cd85a0ca8429538dec8d52a41baa
Alexey Bulenkov <alex@bulenkov.com>
2019-02-16 15:21:10 +0300

    implement field generator

--- src/minesweeper/main.kt
+++ src/minesweeper/main.kt
@@ -1,5 +1,43 @@
 package minesweeper
 
+import java.util.*
+
+const val fieldCeilCount = 81;
+
 fun main(args: Array<String>) {
-    print("Hello world!")
+    val scanner = Scanner(System.`in`)
+
+    print("How many mines do you want on the field? ")
+
+    var count = scanner.nextInt()
+
+    if (count > fieldCeilCount) {
+        print("Invalid mines count!")
+
+        return
+    }
+
+    val random = Random()
+    val field = BooleanArray(fieldCeilCount)
+
+    while (count > 0) {
+        val index = random.nextInt(fieldCeilCount - 1)
+
+        if (!field[index]) {
+            field[index] = true
+            count--
+        }
+    }
+
+    for (i in 0..(fieldCeilCount - 1)) {
+        if (i != 0 && i % 9 == 0) {
+            println()
+        }
+
+        if (field[i]) {
+            print('X')
+        } else {
+            print('.')
+        }
+    }
 }
