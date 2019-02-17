74e2625bd7ebeba0171cd309c1194da281f40d59
Izmaylov Alexandr <zinoviy23@gmail.com>
2019-02-17 23:55:31 +0300

    update Field and other after-review fixes

--- src/minesweeper/main.kt
+++ src/minesweeper/main.kt
@@ -9,155 +9,273 @@ const val EMPTY_CELL_CHAR = '\\'
 const val MARK_CHAR = '*'
 
 class FieldInfo(
-    val field: Array<CharArray>,
-    val mineCount: Int,
-    val marks: MutableSet<Pair<Int, Int>>,
-    val opened: MutableSet<Pair<Int, Int>>
-)
+    private val fieldSize: FieldSize,
+    val mineCount: Int
+) {
+    val rowIndices: IntRange
+        get() = 0 until fieldSize.rows
 
-fun generateMines(fieldInfo: FieldInfo, generationPoint: Pair<Int, Int>) {
-    var remainingMines = fieldInfo.mineCount
-    for (rowId in fieldInfo.field.indices) {
-        for (colId in fieldInfo.field[0].indices) {
-            val remainingCellsCount = fieldInfo.field.size * fieldInfo.field[0].size -
-                    rowId * fieldInfo.field.size - colId
+    val colIndices: IntRange
+        get() = 0 until fieldSize.cols
 
-            if (rowId == generationPoint.first && colId == generationPoint.second) {
+    val opened = mutableSetOf<CellPosition>()
+
+    val marks = mutableSetOf<CellPosition>()
+
+    val mines = mutableSetOf<CellPosition>()
+
+    val rows = fieldSize.rows
+
+    val cols = fieldSize.cols
+
+    val cells: Iterable<CellPosition>
+        get() = object: Iterable<CellPosition> {
+            override fun iterator(): Iterator<CellPosition> {
+                return object: Iterator<CellPosition> {
+                    var currentCell = CellPosition(0, -1)
+
+                    override fun hasNext(): Boolean {
+                        return currentCell.rowId != rows - 1 || currentCell.colId != cols - 1
+                    }
+
+                    override fun next(): CellPosition {
+                        var colId = currentCell.colId + 1
+                        val rowId: Int
+
+                        if (colId == cols) {
+                            colId = 0
+                            rowId = currentCell.rowId + 1
+                        } else {
+                            rowId = currentCell.rowId
+                        }
+
+                        currentCell = CellPosition(rowId, colId)
+
+                        return currentCell
+                    }
+                }
+            }
+        }
+
+    val minesAroundCells: Array<IntArray> = Array(rows) { IntArray(cols) }
+}
+
+
+data class FieldSize(val rows: Int, val cols: Int)
+data class CellPosition(val rowId: Int, val colId: Int)
+
+fun FieldInfo.generateMines(generationPoint: CellPosition) {
+    var remainingMines = mineCount
+    for (rowId in rowIndices) {
+        for (colId in colIndices) {
+            val remainingCellsCount = rows * cols - rowId * rows - colId
+
+            if (rowId == generationPoint.rowId && colId == generationPoint.colId) {
                 if (remainingMines >= remainingCellsCount) {
-                    val firstNotMine = findFirstNoMine(fieldInfo)
-                    fieldInfo.field[firstNotMine.first][firstNotMine.second] = MINE_CHAR
+                    val firstNotMine = checkNotNull(findFirstCellNotMine(this)) {
+                        "Internal error! Field with size: ($rows, $cols)" +
+                                " cannot contain $mineCount mines"
+                    }
+                    mines += CellPosition(firstNotMine.rowId, firstNotMine.colId)
                     remainingMines--
                 }
                 continue
             }
 
-            if (remainingMines > 0 &&
-                (Random.nextInt(fieldInfo.field.size * fieldInfo.field[0].size - 1) <= fieldInfo.mineCount ||
+            if (remainingMines > 0 && (Random.nextInt(rows * cols - 1) <= mineCount ||
                         remainingMines == remainingCellsCount)
             ) {
                 remainingMines--
-                fieldInfo.field[rowId][colId] = MINE_CHAR
-            } else
-                fieldInfo.field[rowId][colId] = HIDDEN_CELL_CHAR
+                mines += CellPosition(rowId, colId)
+            }
         }
     }
 }
 
-fun findFirstNoMine(fieldInfo: FieldInfo): Pair<Int, Int> {
-    for (rowId in fieldInfo.field.indices)
-        for (colId in fieldInfo.field[0].indices)
-            if (fieldInfo.field[rowId][colId] != MINE_CHAR)
-                return Pair(rowId, colId)
+fun findFirstCellNotMine(fieldInfo: FieldInfo): CellPosition? {
+    for (rowId in fieldInfo.rowIndices) {
+        for (colId in fieldInfo.colIndices) {
+            val currentCell = CellPosition(rowId, colId)
 
-    return Pair(-1, -1)
+            if (currentCell !in fieldInfo.mines) {
+                return currentCell
+            }
+        }
+    }
+
+    return null
 }
 
 fun countMines(fieldInfo: FieldInfo) {
-    for (rowId in fieldInfo.field.indices)
-        for (colId in fieldInfo.field[0].indices)
-            if (fieldInfo.field[rowId][colId] != MINE_CHAR) {
-                val mineCount = countMinesAroundCell(fieldInfo, rowId, colId)
-                if (mineCount != 0)
-                    fieldInfo.field[rowId][colId] = '0' + mineCount
-                else
-                    fieldInfo.field[rowId][colId] = EMPTY_CELL_CHAR
-            }
+    for (cell in fieldInfo.cells) {
+        if (cell !in fieldInfo.mines) {
+            fieldInfo.minesAroundCells[cell.rowId][cell.colId] =
+                countMinesAroundCell(fieldInfo, cell.rowId, cell.colId)
+        }
+    }
 }
 
 fun countMinesAroundCell(fieldInfo: FieldInfo, rowId: Int, colId: Int): Int {
     val deltas = intArrayOf(-1, 0, 1)
 
     var cnt = 0
-    for (dRowId in deltas)
-        for (dColId in deltas)
+    for (dRowId in deltas) {
+        for (dColId in deltas) {
             if (!(dRowId == 0 && dColId == 0) &&
-                rowId + dRowId in fieldInfo.field.indices &&
-                colId + dColId in fieldInfo.field[0].indices &&
-                fieldInfo.field[dRowId + rowId][dColId + colId] == MINE_CHAR
+                rowId + dRowId in fieldInfo.rowIndices &&
+                colId + dColId in fieldInfo.colIndices &&
+                CellPosition(rowId + dRowId, colId + dColId) in fieldInfo.mines
             ) {
                 cnt++
             }
+        }
+    }
 
     return cnt
 }
 
-fun printField(fieldInfo: FieldInfo, showAllCells: Boolean) {
-    val fieldToPrint = fieldInfo.field.map { it.copyOf() }.toTypedArray()
+fun colIdToString(id: Int) = "${'a' + id}"
 
-    if (!showAllCells) {
-        for (rowId in fieldInfo.field.indices)
-            for (colId in fieldInfo.field[0].indices)
-                if (Pair(rowId, colId) !in fieldInfo.opened)
-                    fieldToPrint[rowId][colId] = HIDDEN_CELL_CHAR
+fun printField(fieldInfo: FieldInfo, showAllHiddenCells: Boolean) {
+    val fieldToPrint = Array(fieldInfo.rows) { CharArray(fieldInfo.cols) }
 
-        for ((rowId, colId) in fieldInfo.marks) {
-            fieldToPrint[rowId][colId] = MARK_CHAR
+    for (cell in fieldInfo.cells) {
+        fieldToPrint[cell.rowId][cell.colId] = when (cell) {
+            in fieldInfo.mines -> MINE_CHAR
+            else ->
+                if (fieldInfo.minesAroundCells[cell.rowId][cell.colId] != 0)
+                    '0' + fieldInfo.minesAroundCells[cell.rowId][cell.colId]
+                else
+                    EMPTY_CELL_CHAR
         }
     }
 
-    println(" |${('a'..('a' + fieldInfo.field[0].size - 1)).joinToString("")}|")
-    println("-|----------|")
+    if (!showAllHiddenCells) {
+        for (cell in fieldInfo.cells) {
+            fieldToPrint[cell.rowId][cell.colId] = when (cell) {
+                in fieldInfo.marks -> MARK_CHAR
+                !in fieldInfo.opened -> HIDDEN_CELL_CHAR
+                else -> fieldToPrint[cell.rowId][cell.colId]
+            }
+        }
+    }
+
+    val indent = fieldInfo.cols.toString().length
+    println("${" ".repeat(indent)}|${fieldInfo.rowIndices.joinToString("") { colIdToString(it) }}|")
+    println("${"-".repeat(indent)}|${"-".repeat(fieldInfo.rows)}|")
+
     println(fieldToPrint.withIndex().joinToString("\n") { (rowId, row) ->
-        ('a' + rowId).toString() + row.joinToString(separator = "", prefix = "|", postfix = "|")
+        "%${indent}d".format(rowId + 1) + row.joinToString(separator = "", prefix = "|", postfix = "|")
     })
-    println("-|----------|")
+
+    println("${"-".repeat(indent)}|${"-".repeat(fieldInfo.rows)}|")
 }
 
-fun openCell(fieldInfo: FieldInfo, point: Pair<Int, Int>): Boolean {
-    if (point in fieldInfo.opened)
+fun openCell(fieldInfo: FieldInfo, cell: CellPosition): Boolean {
+    if (cell in fieldInfo.opened) {
         return false
+    }
 
-    if (fieldInfo.field[point.first][point.second] == MINE_CHAR)
+    if (cell in fieldInfo.mines) {
         return true
+    }
 
-    if (point in fieldInfo.marks)
-        fieldInfo.marks -= point
+    if (cell in fieldInfo.marks) {
+        fieldInfo.marks -= cell
+    }
 
-    fieldInfo.opened += point
-    if (fieldInfo.field[point.first][point.second] == EMPTY_CELL_CHAR)
-        openNeighbours(fieldInfo, point)
+    fieldInfo.opened += cell
+    if (fieldInfo.minesAroundCells[cell.rowId][cell.colId] == 0) {
+        openNeighbours(fieldInfo, cell)
+    }
 
     return false
 }
 
-fun openNeighbours(fieldInfo: FieldInfo, point: Pair<Int, Int>) {
+fun openNeighbours(fieldInfo: FieldInfo, cell: CellPosition) {
     val deltas = intArrayOf(-1, 0, 1)
 
-    for (dRowId in deltas)
+    for (dRowId in deltas) {
         for (dColId in deltas) {
-            if (point.first + dRowId !in fieldInfo.field.indices ||
-                point.second + dColId !in fieldInfo.field[0].indices
+            if (cell.rowId + dRowId !in fieldInfo.rowIndices ||
+                cell.colId + dColId !in fieldInfo.colIndices
             ) {
                 continue
             }
 
-            if (Pair(point.first + dRowId, point.second + dColId) !in fieldInfo.opened)
-                openCell(fieldInfo, Pair(point.first + dRowId, point.second + dColId))
+            val newCellToOpen = CellPosition(cell.rowId + dRowId, cell.colId + dColId)
+            if (newCellToOpen !in fieldInfo.opened) {
+                openCell(fieldInfo, newCellToOpen)
+            }
         }
+    }
 }
 
-fun checkGame(fieldInfo: FieldInfo): Boolean {
-    if (fieldInfo.opened.size == fieldInfo.field.size * fieldInfo.field[0].size - fieldInfo.mineCount)
+fun checkGameToWin(fieldInfo: FieldInfo): Boolean {
+    if (fieldInfo.opened.size == fieldInfo.rows * fieldInfo.cols - fieldInfo.mineCount) {
         return true
+    }
 
-    if (fieldInfo.marks.size != fieldInfo.mineCount)
+    if (fieldInfo.marks.size != fieldInfo.mineCount) {
         return false
+    }
 
-    for ((rowId, colId) in fieldInfo.marks) {
-        if (fieldInfo.field[rowId][colId] != MINE_CHAR)
+    for (markedCell in fieldInfo.marks) {
+        if (markedCell !in fieldInfo.cells) {
             return false
+        }
     }
 
     return true
 }
 
-fun mark(fieldInfo: FieldInfo, point: Pair<Int, Int>) {
-    if (point !in fieldInfo.opened)
-        fieldInfo.marks += point
+fun mark(fieldInfo: FieldInfo, cell: CellPosition) {
+    if (cell !in fieldInfo.opened) {
+        fieldInfo.marks += cell
+    }
 }
 
 const val FIELD_SIZE = 10
 const val MAX_MINES_COUNT = FIELD_SIZE * FIELD_SIZE - 1
+val WHITESPACES = "[\\t ]+".toRegex()
+
+enum class Command(private val stringValue: String) {
+    FREE("free"),
+    MARK("mark");
+
+    override fun toString() = stringValue
+}
+
+data class UserInput(val cell: CellPosition, val cmd: String?)
+
+fun Scanner.readUserCommand(msg: String): UserInput? {
+    var cmd: String? = null
+    var rowId: Int? = null
+    var colId: Int? = null
+
+    do {
+        print(msg)
+
+        val enteredValues = WHITESPACES.split(nextLine() ?: return null)
+
+        if (enteredValues.size < 2 || enteredValues[0].isEmpty() || enteredValues[1].isEmpty())
+            continue
+
+        rowId = enteredValues[0].toIntOrNull() ?: enteredValues[1].toIntOrNull()
+
+        colId = when {
+            enteredValues[1].first() in 'a'..'z' -> enteredValues[1].first() - 'a'
+            enteredValues[0].first() in 'a'..'z' -> enteredValues[0].first() - 'a'
+            else -> null
+        }
+
+        if (enteredValues.size > 2)
+            cmd = enteredValues[2]
+
+    } while (rowId == null || colId == null)
+
+    return UserInput(CellPosition(rowId - 1, colId), cmd)
+}
 
 fun main() {
     val scanner = Scanner(System.`in`)
@@ -168,43 +286,35 @@ fun main() {
         mineCount = scanner.nextInt()
     } while (mineCount !in 1..MAX_MINES_COUNT)
 
-    val fieldInfo = FieldInfo(
-        Array(FIELD_SIZE) { CharArray(FIELD_SIZE) },
-        mineCount,
-        mutableSetOf(),
-        mutableSetOf()
-    )
+    val fieldInfo = FieldInfo(FieldSize(FIELD_SIZE, FIELD_SIZE), mineCount)
 
     printField(fieldInfo, false)
-    var generationPoint: Pair<Int, Int>
+    var generationPoint: CellPosition
     do {
-        print("Choose first cell to free: ")
-        val x = scanner.next().first()
-        val y = scanner.next().first()
-        generationPoint = Pair(y - 'a', x - 'a')
-    } while (generationPoint.first !in fieldInfo.field.indices ||
-        generationPoint.second !in fieldInfo.field[0].indices
+        val (cell, _) = scanner.readUserCommand("Choose first cell to free: ") ?: return
+        generationPoint = cell
+    } while (generationPoint.rowId !in fieldInfo.rowIndices ||
+        generationPoint.colId !in fieldInfo.colIndices
     )
 
-    generateMines(fieldInfo, generationPoint)
+    fieldInfo.generateMines(generationPoint)
     countMines(fieldInfo)
     openCell(fieldInfo, generationPoint)
     printField(fieldInfo, false)
 
     var isUserFail = false
-    while (!checkGame(fieldInfo) && !isUserFail) {
-        print("Set/unset mines marks or claim a cell as free(coordinates and command): ")
-        val x = scanner.next().first()
-        val y = scanner.next().first()
-        val cmd = scanner.next()
+    while (!checkGameToWin(fieldInfo) && !isUserFail) {
+        val (cell, cmd) = scanner.readUserCommand(
+            "Set/unset mines marks or claim a cell as free(coordinates and command): "
+        ) ?: return
 
-        val point = Pair(y - 'a', x - 'a')
+        println("$cell $cmd")
 
-        if (point.first in fieldInfo.field.indices && point.second in fieldInfo.field[0].indices) {
+        if (cell.rowId in fieldInfo.rowIndices && cell.rowId in fieldInfo.rowIndices) {
             isUserFail = when (cmd) {
-                "free" -> openCell(fieldInfo, point)
-                "mark" -> {
-                    mark(fieldInfo, point)
+                Command.FREE.toString() -> openCell(fieldInfo, cell)
+                Command.MARK.toString() -> {
+                    mark(fieldInfo, cell)
                     false
                 }
                 else -> false
