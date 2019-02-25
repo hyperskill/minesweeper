a36ab600712b1d2a9d0097126fd2884ea7642dec
DjMatesha <dj_matesha@mail.ru>
2019-02-25 19:20:55 +0300

    Update main.kt

--- src/minesweeper/main.kt
+++ src/minesweeper/main.kt
@@ -1,5 +1,29 @@
 package minesweeper
 
+import java.util.Scanner
+import java.util.Random
+
+var random = Random()
+
 fun main(args: Array<String>) {
-    print("Hello world!")
+    val len = 9
+    val map: Array<Array<Char>> = Array(len, { Array(len, { '.' }) })
+    val scanner = Scanner(System.`in`)
+    var x: Int
+    var y: Int
+    var n: Int
+    do {
+        print("How many mines do you want on the field? ")
+        n = scanner.nextInt()
+    } while (n < 0 || n > len * len)
+    for (i in 1..n) {
+        do {
+            x = random.nextInt(len)
+            y = random.nextInt(len)
+        } while (map[x][y] == 'X')
+        map[x][y] = 'X'
+    }
+    for (i in map) {
+        println(i.joinToString(separator = ""))
+    }
 }
