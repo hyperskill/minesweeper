a839a96231f85d6ce7aa2c5f804d9e5ea644edef
DjMatesha <dj_matesha@mail.ru>
2019-03-05 19:29:56 +0300

    Update main.kt

--- src/minesweeper/main.kt
+++ src/minesweeper/main.kt
@@ -7,11 +7,16 @@ const val SIZE = 9
 const val SPACE = '.'
 const val MINE = 'X'
 const val MARK = '*'
+const val OPEN = '/'
 var random = Random()
+val scanner = Scanner(System.`in`)
 
 fun generateMap(map: Array<Array<Char>>, n: Int) {
     var x: Int
     var y: Int
+    for (i in 1..SIZE)
+        for (j in 1..SIZE)
+            map[i][j] = SPACE
     for (i in 1..n) {
         do {
             x = random.nextInt(SIZE) + 1
@@ -45,58 +50,102 @@ fun Array<Array<Char>>.printMap() {
         println(this[i].sliceArray(1..SIZE).joinToString(separator = "",prefix = "$i|", postfix = "|"))
     println("-|---------|")
 }
-fun Array<Array<Char>>.getMines(a: Char, b: Char): Set<Pair<Int,Int>> {
+fun Array<Array<Char>>.getMarks (a: Char): Set<Pair<Int,Int>> {
     var mines: MutableList<Pair<Int,Int>> = arrayListOf()
-    for (i in 0 until  this.count())
-        for (j in 0 until  this[i].count())
+    for (i in 1..SIZE)
+        for (j in 1..SIZE)
         if (this[i][j] == a){
-            mines.add(Pair(j,i))
-            this[i][j] = b
+            mines.add(Pair(i,j))
         }
     return mines.toSet()
 }
 fun Array<Array<Char>>.setMark(mine: Pair<Int, Int>, c: Char){
-    this[mine.second][mine.first]=c
+    this[mine.first][mine.second]=c
+}
+fun Array<Array<Char>>.getCell(cell : Pair<Int, Int>) = this[cell.first][cell.second]
+fun Array<Array<Char>>.explore(cell : Pair<Int, Int>, mapPrint: Array<Array<Char>>){
+    val indexes = arrayOf(-1, 0, 1)
+    when (this.getCell(cell)){
+        SPACE ->
+        {
+            this.setMark(cell, OPEN)
+            mapPrint.setMark(cell, OPEN)
+            for (k in indexes)
+                for (p in indexes)
+                    explore(Pair(cell.first+k, cell.second+p), mapPrint)
+        }
+        in '1'..'9' ->{
+            mapPrint.setMark(cell, this.getCell(cell))
+        }
+
+    }
+}
+
+fun Array<Array<Char>>.allCellsOpen(mines: Set<Pair<Int, Int>>): Boolean{
+    var set = this.getMarks(SPACE).toMutableSet()
+    set.addAll(this.getMarks(MARK))
+    return mines==set
 }
 
 fun play(map: Array<Array<Char>>, mines: Set<Pair<Int,Int>>){
+    val mapPrint: Array<Array<Char>> = Array(SIZE + 2, { Array(SIZE + 2, { SPACE }) })
+    mapPrint.printMap()
     var marks: MutableSet<Pair<Int,Int>> = mutableSetOf()
-    while (!marks.equals(mines)){
-        var set: Pair<Int, Int>? = null
+    while (!marks.equals(mines) && !mapPrint.allCellsOpen(mines)){
+        var cell: Pair<Int, Int>? = null
         do{
-            print("Set/delete mines marks (x and y coordinates): ")
-            val t = readLine()!!.split(" ").map(String::toInt)
-            if (t.count()==2 && t[0] in 1..SIZE && t[1] in 1..SIZE ){
-                when (map[t[1]][t[0]]){
-                    SPACE -> {
-                        set = Pair(t[0],t[1])
-                        marks.add(set)
-                        map.setMark(set, MARK)
+            print("Set/unset mines marks (x and y coordinates): ")
+            val n1 = scanner.nextInt()
+            val n2 = scanner.nextInt()
+            val str = scanner.next()
+            if ( n1 in 1..SIZE && n2 in 1..SIZE ){
+                cell = Pair(n2,n1)
+                when (str){
+                    "free" -> {
+                        when (map.getCell(cell)) {
+                            SPACE -> map.explore(cell, mapPrint)
+                            in '1'..'9' ->{
+                                mapPrint.setMark(cell, map.getCell(cell))
+                            }
+                            MINE -> {
+                                println()
+                                println("You stepped on a mine and failed!")
+                                return
+                            }
+                        }                                                
                     }
-                    MARK ->{
-                        set = Pair(t[0],t[1])
-                        marks.remove(set)
-                        map.setMark(set, SPACE)
+                    "mine" -> {
+                        when (mapPrint.getCell(cell)){
+                            SPACE -> {
+                                marks.add(cell)
+                                mapPrint.setMark(cell,MARK)
+                            }
+                            MARK ->{
+                                marks.remove(cell)
+                                mapPrint.setMark(cell, SPACE)
+                            }
+                            else -> cell = null
+                        }
                     }
+                    else -> cell = null
                 }
             }
-        } while ( set == null)
-        map.printMap()
+        } while ( cell == null)
+        mapPrint.printMap()
     }
     println()
-    println("Congratulations! You founded all mines!\n")
+    println("Congratulations! You founded all mines!")
 }
 
 fun main(args: Array<String>) {
-    val map: Array<Array<Char>> = Array(SIZE + 2, { Array(SIZE + 2, { SPACE }) })
+    val map: Array<Array<Char>> = Array(SIZE + 2, { Array(SIZE + 2, { OPEN }) })
     var n: Int
     do {
         print("How many mines do you want on the field? ")
-        n = readLine()!!.toInt()
+        n = scanner.nextInt()
     } while (n < 0 || n > SIZE * SIZE)
     generateMap(map, n)
     calculateMap(map)
-    val mines = map.getMines(MINE,SPACE)
-    map.printMap()
+    val mines = map.getMarks(MINE)
     play(map,mines)
 }
