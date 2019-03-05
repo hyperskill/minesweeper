fa9d349f813a32138190b56d4d099e8579f2f916
Alexey Bulenkov <alex@bulenkov.com>
2019-03-06 02:59:02 +0300

    complete the game

--- src/minesweeper/main.kt
+++ src/minesweeper/main.kt
@@ -7,6 +7,7 @@ const val cols = 9
 const val mine = 'X'
 const val mark = '*'
 const val markedMine = '+'
+const val discovered = '/'
 const val empty = '.'
 
 fun generateField(minesCount:Int): Array<Array<Char?>> {
@@ -81,8 +82,8 @@ fun printField(field:Array<Array<Char?>>) {
         for (col in 0..field[row].lastIndex) {
             when(field[row][col]) {
                 mark, markedMine -> print(mark)
-                mine -> print(empty)
-                null -> print(cellInfo(field, row, col))
+                mine, null -> print(empty)
+                else -> print(field[row][col])
             }
         }
 
@@ -92,24 +93,86 @@ fun printField(field:Array<Array<Char?>>) {
     printSeparate(cols)
 }
 
-fun markCell(field: Array<Array<Char?>>, rowIndex:Int, colIndex:Int) {
-    field[rowIndex][colIndex] = when(field[rowIndex][colIndex]) {
+fun freeCell(field: Array<Array<Char?>>, row: Int, col: Int) {
+    if (field[row][col] != null) {
+        return
+    }
+
+    val info = cellInfo(field, row, col)
+
+    if (info != empty) {
+        field[row][col] = info
+
+        return
+    }
+
+    field[row][col] = discovered
+
+    val rowRange = 0 until rows
+    val colRange = 0 until cols
+
+    if((row + 1) in rowRange) {
+        freeCell(field, row + 1, col)
+    }
+
+    if ((row + 1) in rowRange && (col + 1) in colRange) {
+        freeCell(field, row + 1, col + 1)
+    }
+
+    if ((col + 1) in 0 until cols) {
+        freeCell(field, row, col + 1)
+    }
+
+    if ((row - 1) in rowRange && (col + 1) in colRange) {
+        freeCell(field, row - 1, col + 1)
+    }
+
+    if ((row - 1) in rowRange) {
+        freeCell(field, row - 1, col)
+    }
+
+    if ((row - 1) in rowRange && (col - 1) in colRange) {
+        freeCell(field, row - 1, col - 1)
+    }
+
+    if ((col - 1) in colRange) {
+        freeCell(field, row, col - 1)
+    }
+
+    if ((row + 1) in rowRange && (col - 1) in colRange) {
+        freeCell(field, row + 1, col - 1)
+    }
+}
+
+fun markCell(field: Array<Array<Char?>>, row:Int, col:Int) {
+    field[row][col] = when(field[row][col]) {
         mine -> markedMine
         markedMine -> mine
+        mark -> null
         else -> mark
     }
 }
 
 fun isWin(field: Array<Array<Char?>>): Boolean {
+    var minesCount = 0
+    var markCount = 0
+    var emptyCellCount = 0
+
     for (row in 0..field.lastIndex) {
         for (col in 0..field[row].lastIndex) {
-            if (field[row][col] == mine) {
-                return false
+            when(field[row][col]) {
+                mine -> minesCount++
+                mark -> markCount++
+                null -> emptyCellCount++
             }
         }
     }
 
-    return true
+    if (markCount > 0) {
+        return false
+    }
+
+    return minesCount == 0 || emptyCellCount == 0
 }
 
 fun main(args: Array<String>) {
@@ -124,10 +187,23 @@ fun main(args: Array<String>) {
         printField(field)
         print("Set/delete mines marks (x and y coordinates): ")
 
-        val x = scanner.next().toUpperCase()
-        val y = scanner.nextInt()
+        val col = scanner.next().toUpperCase()[0].toInt() - 'A'.toInt()
+        val row = scanner.nextInt() - 1
+        val action = scanner.next().toLowerCase()
 
-        markCell(field, y - 1, x[0].toInt() - 'A'.toInt())
+        when(action) {
+            "free" -> {
+                if (field[row][col] == mine) {
+                    println("Game over!")
+
+                    return
+                }
+
+                freeCell(field, row, col)
+            }
+            "mine" -> markCell(field, row, col)
+            else -> println("Unknown action")
+        }
 
         if (isWin(field)) {
             break;
